f3df939c4a916e577e590bc9c017b3539c2cf392
Russ Cox <rsc@golang.org>
2016-11-02 23:37:53 -0400

    initial commit

---
+++ a/a.go
@@ -0,0 +1,4 @@
+package a
+
+import _ "github.com/rsc/go-get-issue-17648-a/b"
+import _ "github.com/rsc/go-get-issue-17648-b/x"
---
+++ b/b.go
@@ -0,0 +1,3 @@
+package b
+
+import _ "github.com/rsc/go-get-issue-17648-a/c"
